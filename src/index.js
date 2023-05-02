cc81e54da0082841b8cc437868d4a7b161e83f62
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-05-02 12:48:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const updateFile = async (fileName, fileContent) => {
+  await fs.updateFile(fileName,'An online learning platform.');
   // write code here
   // dont change function name
   
